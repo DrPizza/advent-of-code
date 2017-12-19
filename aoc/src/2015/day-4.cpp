a177db5ea9a3431aacb832c65ef3f778f4bbf838
Peter Bright <drpizza@quiscalusmexicanus.org>
2017-12-18 22:25:21 -0500

    2015-5

--- aoc/src/2015/day-4.cpp
+++ aoc/src/2015/day-4.cpp
@@ -3,6 +3,15 @@
 #include "problem.hpp"
 
 #include <fstream>
+#include <sstream>
+#include <array>
+
+#pragma warning(push)
+
+#pragma warning(disable: 4619 4365 26432 26440 26439 26474 26495 26497 26493 26492 26481 26494 26472 26429 26481 26409 26466 26424 26434 26496)
+#define CRYPTOPP_ENABLE_NAMESPACE_WEAK 1
+#include <cryptopp/md5.h>
+#pragma warning(pop)
 
 struct advent_2015_4 : problem
 {
@@ -10,9 +19,42 @@ struct advent_2015_4 : problem
 	}
 
 protected:
+	std::string key;
+
 	void prepare_input(std::ifstream& fin) override {
-		std::string line;
-		std::getline(fin, line);
+		std::getline(fin, key);
+	}
+
+	std::string part_1() override {
+		CryptoPP::Weak::MD5 md5;
+		std::array<uint8_t, 16> digest{};
+		std::size_t i = 0;
+		for(; ; ++i) {
+			std::string trial = key + std::to_string(i);
+			md5.CalculateDigest(digest.data(), reinterpret_cast<const byte*>(trial.data()), trial.size());
+			if(digest[0] == 0x00
+			&& digest[1] == 0x00
+			&& digest[2] <= 0x0f) {
+				break;
+			}
+		}
+		return std::to_string(i);
+	}
+
+	std::string part_2() override {
+		CryptoPP::Weak::MD5 md5;
+		std::array<uint8_t, 16> digest{};
+		std::size_t i = 0;
+		for(; ; ++i) {
+			std::string trial = key + std::to_string(i);
+			md5.CalculateDigest(digest.data(), reinterpret_cast<const byte*>(trial.data()), trial.size());
+			if(digest[0] == 0x00
+			&& digest[1] == 0x00
+			&& digest[2] == 0x00) {
+				break;
+			}
+		}
+		return std::to_string(i);
 	}
 };
 
--- aoc/src/2015/day-5.cpp
+++ aoc/src/2015/day-5.cpp
@@ -3,6 +3,7 @@
 #include "problem.hpp"
 
 #include <fstream>
+#include <algorithm>
 
 struct advent_2015_5 : problem
 {
@@ -10,9 +11,66 @@ struct advent_2015_5 : problem
 	}
 
 protected:
+	std::vector<std::string> strings;
+
 	void prepare_input(std::ifstream& fin) override {
-		std::string line;
-		std::getline(fin, line);
+		for(std::string line; std::getline(fin, line); ) {
+			strings.push_back(line);
+		}
+	}
+
+	std::string part_1() override {
+		const auto is_nice = [](const std::string& s) -> bool {
+			if(s.find("ab") != -1
+			|| s.find("cd") != -1
+			|| s.find("pq") != -1
+			|| s.find("xy") != -1) {
+				return false;
+			}
+			const std::ptrdiff_t vowel_count = std::count_if(std::begin(s), std::end(s), [](char c) {
+				return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+			});
+			if(vowel_count < 3) {
+				return false;
+			}
+			if(std::adjacent_find(std::begin(s), std::end(s)) == std::end(s)) {
+				return false;
+			}
+			return true;
+		};
+		std::ptrdiff_t nice = std::count_if(std::begin(strings), std::end(strings), is_nice);
+		return std::to_string(nice);
+	}
+
+	std::string part_2() override {
+		const auto is_nice = [](const std::string& s) -> bool {
+			bool has_repeat = false;
+			for(std::size_t i = 0; !has_repeat && i < s.size() - 3; ++i) {
+				for(std::size_t j = i + 2; j < s.size() - 1; ++j) {
+					if(s[i    ] == s[j    ]
+					&& s[i + 1] == s[j + 1]) {
+						has_repeat = true;
+						break;
+					}
+				}
+			}
+			if(!has_repeat) {
+				return false;
+			}
+			bool has_aba = false;
+			for(std::size_t i = 0; i < s.size() - 2; ++i) {
+				if(s[i] == s[i + 2]) {
+					has_aba = true;
+					break;
+				}
+			}
+			if(!has_aba) {
+				return false;
+			}
+			return true;
+		};
+		std::ptrdiff_t nice = std::count_if(std::begin(strings), std::end(strings), is_nice);
+		return std::to_string(nice);
 	}
 };
 
