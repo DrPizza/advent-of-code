e1ae7384e0d1b0aee4d8267d0cc2ad956ef199cf
Peter Bright <drpizza@quiscalusmexicanus.org>
2017-12-18 19:00:33 -0500

    2015 day 3

--- aoc/src/2015/day-2.cpp
+++ aoc/src/2015/day-2.cpp
@@ -4,6 +4,7 @@
 
 #include <fstream>
 #include <functional>
+#include <vector>
 
 #include <boost/algorithm/string.hpp>
 
--- aoc/src/2015/day-3.cpp
+++ aoc/src/2015/day-3.cpp
@@ -3,6 +3,26 @@
 #include "problem.hpp"
 
 #include <fstream>
+#include <functional>
+#include <unordered_map>
+
+namespace {
+	struct coord
+	{
+		std::ptrdiff_t x, y;
+	};
+
+	static bool operator==(const coord& lhs, const coord& rhs) noexcept {
+		return lhs.x == rhs.x && lhs.y == rhs.y;
+	}
+
+	struct coord_hash
+	{
+		std::size_t operator()(const coord& c) const noexcept {
+			return std::hash<std::ptrdiff_t>{}(c.x) ^ std::hash<std::ptrdiff_t>{}(c.y);
+		}
+	};
+}
 
 struct advent_2015_3 : problem
 {
@@ -10,10 +30,69 @@ struct advent_2015_3 : problem
 	}
 
 protected:
+	std::string directions;
+
 	void prepare_input() override {
 		std::ifstream fin("input/2015/day-3.txt");
-		std::string line;
-		std::getline(fin, line);
+		std::getline(fin, directions);
+	}
+
+	std::string part_1() override {
+		std::unordered_map<coord, std::size_t, coord_hash> present_counts;
+		coord position = { 0, 0 };
+		present_counts[position] = 1;
+		for(const char ch : directions) {
+			switch(ch) {
+			case '<':
+				--position.x;
+				++present_counts[position];
+				break;
+			case '^':
+				++position.y;
+				++present_counts[position];
+				break;
+			case '>':
+				++position.x;
+				++present_counts[position];
+				break;
+			case 'v':
+				--position.y;
+				++present_counts[position];
+				break;
+			}
+		}
+		return std::to_string(present_counts.size());
+	}
+
+	std::string part_2() override {
+		std::unordered_map<coord, std::size_t, coord_hash> presents;
+		coord positions[2] = { { 0, 0 }, {0, 0} };
+		++presents[positions[0]];
+		++presents[positions[1]];
+
+		std::size_t idx = 0;
+		for(const char ch : directions) {
+			switch(ch) {
+			case '<':
+				--positions[idx].x;
+				++presents[positions[idx]];
+				break;
+			case '^':
+				++positions[idx].y;
+				++presents[positions[idx]];
+				break;
+			case '>':
+				++positions[idx].x;
+				++presents[positions[idx]];
+				break;
+			case 'v':
+				--positions[idx].y;
+				++presents[positions[idx]];
+				break;
+			}
+			idx = 1 - idx;
+		}
+		return std::to_string(presents.size());
 	}
 };
 
