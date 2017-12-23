1dcd46795b6b997c8e7f50f47ed4b7bc4662d2eb
Peter Bright <drpizza@quiscalusmexicanus.org>
2017-12-22 20:36:00 -0500

    2016/6

--- aoc/src/2016/day-6.cpp
+++ aoc/src/2016/day-6.cpp
@@ -3,6 +3,7 @@
 #include "problem.hpp"
 
 #include <fstream>
+#include <map>
 
 struct advent_2016_6 : problem
 {
@@ -10,9 +11,43 @@ struct advent_2016_6 : problem
 	}
 
 protected:
+	std::vector<std::string> signals;
+
 	void prepare_input(std::ifstream& fin) override {
-		std::string line;
-		std::getline(fin, line);
+		for(std::string line; std::getline(fin, line);) {
+			signals.push_back(line);
+		}
+	}
+
+	std::string most_common;
+	std::string least_common;
+
+	void precompute() override {
+		std::vector<std::map<char, std::size_t>> frequencies(signals[0].size());
+		for(const std::string& s : signals) {
+			for(std::size_t i = 0; i < s.size(); ++i) {
+				++frequencies[i][s[i]];
+			}
+		}
+
+		std::vector<std::multimap<std::size_t, char, std::greater<std::size_t>>> inverse_frequencies(frequencies.size());
+		for(std::size_t i = 0; i < frequencies.size(); ++i) {
+			for(const auto& p : frequencies[i]) {
+				inverse_frequencies[i].insert(std::make_pair(p.second, p.first));
+			}
+		}
+		for(const auto& m : inverse_frequencies) {
+			most_common += m.begin()->second;
+			least_common += (--m.end())->second;
+		}
+	}
+
+	std::string part_1() override {
+		return most_common;
+	}
+
+	std::string part_2() override {
+		return least_common;
 	}
 };
 
