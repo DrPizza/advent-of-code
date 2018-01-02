b0f09f6842b49c0c2e21d8d435a99a780545c863
Peter Bright <drpizza@quiscalusmexicanus.org>
2018-01-02 00:33:51 +0000

    2016/15

--- aoc/src/2016/day-15.cpp
+++ aoc/src/2016/day-15.cpp
@@ -3,6 +3,15 @@
 #include "problem.hpp"
 
 #include <fstream>
+#include <map>
+#include <regex>
+#include "utility.hpp"
+
+struct disc
+{
+	std::size_t size;
+	std::size_t initial;
+};
 
 struct advent_2016_15 : problem
 {
@@ -10,9 +19,55 @@ struct advent_2016_15 : problem
 	}
 
 protected:
+	std::map<std::size_t, disc> discs;
+
 	void prepare_input(std::ifstream& fin) override {
-		std::string line;
-		std::getline(fin, line);
+		std::vector<std::string> lines;
+
+		std::regex pattern(R"(Disc #([[:digit:]]+) has ([[:digit:]]+) positions; at time=0, it is at position ([[:digit:]]+).)");
+		for(std::string line; std::getline(fin, line); ) {
+			std::smatch m;
+			std::regex_search(line, m, pattern);
+			discs[std::stoull(m[1].str())] = disc{ std::stoull(m[2].str()), std::stoull(m[3].str()) };
+		}
+	}
+
+	bool can_traverse_discs(std::size_t delay) {
+		return utility::transform_reduce(std::begin(discs),
+		                                 std::end(discs),
+		                                 true,
+		                                 [](const auto& lhs, const auto& rhs) {
+		                                     return lhs && rhs;
+		                                 },
+		                                 [delay](const auto& p) {
+		                                     const std::size_t position = p.first;
+		                                     const disc d = p.second;
+		                                     if((position + delay + d.initial) % d.size == 0) {
+		                                         return true;
+		                                     } else {
+		                                         return false;
+		                                     }
+		                                 }
+		);
+	}
+
+	std::string part_1() override {
+		for(std::size_t delay = 0; ; ++delay) {
+			bool result = can_traverse_discs(delay);
+			if(result) {
+				return std::to_string(delay);
+			}
+		}
+	}
+
+	std::string part_2() override {
+		discs[discs.size() + 1] = disc{ 11, 0 };
+		for(std::size_t delay = 0; ; ++delay) {
+			bool result = can_traverse_discs(delay);
+			if(result) {
+				return std::to_string(delay);
+			}
+		}
 	}
 };
 
