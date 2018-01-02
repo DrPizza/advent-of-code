d2a5ba2609508b6b9e4a78dff31e829d4201a120
Peter Bright <drpizza@quiscalusmexicanus.org>
2018-01-02 00:50:54 +0000

    2016/16

--- aoc/src/2016/day-16.cpp
+++ aoc/src/2016/day-16.cpp
@@ -10,9 +10,45 @@ struct advent_2016_16 : problem
 	}
 
 protected:
+	std::string initial_state;
+
 	void prepare_input(std::ifstream& fin) override {
-		std::string line;
-		std::getline(fin, line);
+		std::getline(fin, initial_state);
+	}
+
+	std::string dragon_curve(const std::string& a) {
+		std::string b;
+		std::transform(std::crbegin(a), std::crend(a), std::back_inserter(b), [](char c) {
+			return (c == '1' ? '0' : '1');
+		});
+		return a + "0" + b;
+	}
+
+	std::string dragon_curve_length(const std::string& seed, std::size_t length) {
+		std::string dragon = seed;
+		while(dragon.size() < length) {
+			dragon = dragon_curve(dragon);
+		}
+		return dragon.substr(0, length);
+	}
+
+	std::string checksum(const std::string& s) {
+		if(s.size() % 2 == 1) {
+			return s;
+		}
+		std::string cksum;
+		for(std::size_t i = 0; i < s.size(); i += 2) {
+			cksum.push_back(s[i] == s[i + 1] ? '1' : '0');
+		}
+		return checksum(cksum);
+	}
+
+	std::string part_1() override {
+		return checksum(dragon_curve_length(initial_state, 272));
+	}
+
+	std::string part_2() override {
+		return checksum(dragon_curve_length(initial_state, 35'651'584));
 	}
 };
 
