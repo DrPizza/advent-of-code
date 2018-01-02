614a43c76ceb59014f7f8730e45c2fd9c53914bc
Peter Bright <drpizza@quiscalusmexicanus.org>
2018-01-02 04:57:15 +0000

    2016/19

--- aoc/src/2016/day-19.cpp
+++ aoc/src/2016/day-19.cpp
@@ -3,6 +3,7 @@
 #include "problem.hpp"
 
 #include <fstream>
+#include <cmath>
 
 struct advent_2016_19 : problem
 {
@@ -10,9 +11,29 @@ struct advent_2016_19 : problem
 	}
 
 protected:
+	std::uint64_t elf_count;
+
 	void prepare_input(std::ifstream& fin) override {
 		std::string line;
 		std::getline(fin, line);
+		elf_count = std::stoull(line);
+	}
+
+	std::string part_1() override {
+		DWORD idx = 0;
+		_BitScanReverse64(&idx, elf_count);
+		std::uint64_t safe_position = elf_count;
+		safe_position = gsl::narrow_cast<std::uint64_t>(safe_position & ((1 << idx) - 1));
+		safe_position = gsl::narrow_cast<std::uint64_t>(safe_position << 1);
+		safe_position = gsl::narrow_cast<std::uint64_t>(safe_position | 1);
+		return std::to_string(safe_position);
+	}
+
+	std::string part_2() override {
+		const double nearest_power_of_three = std::floor(std::log(elf_count - 1) / std::log(3.0));
+		const double pow_three = std::pow(3.0, nearest_power_of_three);
+		const double result = elf_count - pow_three + std::max(elf_count - (2.0 * pow_three), 0.0);
+		return std::to_string(result);
 	}
 };
 
